b7e80007415d8b8980493bc9e9bc343f9b5daaa9
Akanksha <>
2024-06-11 12:03:32 +0800

    Initial Framework

---
+++ Validator/Validator/EmailValidator.swift
@@ -0,0 +1,15 @@
+//
+//  EmailValidator.swift
+//  Validator
+//
+//  Created by Akanksha Thakur on 11/6/24.
+//
+
+import Foundation
+
+public struct CredentialValidator {
+  
+  public static func isValidEmail(_ email: String) -> Bool {
+    return false
+  }
+}
